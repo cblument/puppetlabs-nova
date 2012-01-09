236710db57f134b5b5ced8c0feecddff460ef92d
Chris Blumentritt <chris.blumentritt@rackspace.com>
2012-01-09 16:24:26 -0600

    Adding additional config options for compute

--- manifests/compute/xenserver.pp
+++ manifests/compute/xenserver.pp
@@ -3,16 +3,27 @@ class nova::compute::xenserver(
   $xenapi_connection_url,
   $xenapi_connection_username,
   $xenapi_connection_password,
-  $xenapi_inject_image=false
+  $xenapi_inject_image=false,
+  $notification_driver
 ) {
 
   nova_config {
+    'host': value => $hostname;
     'console_host': value => $console_host;
     'connection_type': value => 'xenapi';
     'xenapi_connection_url': value => $xenapi_connection_url;
     'xenapi_connection_username': value => $xenapi_connection_username;
     'xenapi_connection_password': value => $xenapi_connection_password;
     'xenapi_inject_image': value => $xenapi_inject_image;
+    'running_deleted_instance_timeout': value => '0';
+    'running_deleted_instance_poll_interval': value => '30';
+    'running_deleted_instance_action': value => 'reap';
+    'xenapi_vhd_coalesce_max_attempts': value => '720';
+    'noflat_injected': value => 'false';
+    'instance_name_template': value => 'instance-%(uuid)s';
+    'notification_driver': value => $notification_driver;
+    'xenapi_generate_swap': value => 'true';
+    'glance_num_retries': value => '5';
   }
 
   package { 'xenapi':
